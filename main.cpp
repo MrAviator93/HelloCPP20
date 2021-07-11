14a5dd95fdb9076e8794d4cba31d52fe5cae426c
MrAviator93 <kalninsarturs@hotmail.com>
2021-07-11 23:21:30 +0100

    Changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <concepts>
 #include <cstddef>
 #include <iostream>
+#include <string>
 
 template < typename INT_TYPE >
-concept UnsignedInteger = std::is_integral_v< INT_TYPE >&& std::is_unsigned_v< INT_TYPE >;
+concept UnsignedIntegerType = std::is_integral_v< INT_TYPE >&& std::is_unsigned_v< INT_TYPE >;
 
-template < UnsignedInteger INT_TYPE >
+template < UnsignedIntegerType INT_TYPE >
 constexpr INT_TYPE bit( std::uint8_t shift ) noexcept( false )
 {
 	if( shift == 0 )
@@ -38,7 +39,37 @@ concept Fruit = requires( FRUIT_TYPE f )
 	->std::convertible_to< std::string >;
 };
 
+class Apple
+{
+public:
+	std::string type()
+	{
+		return { "Apple" };
+	}
+};
+
+class Banana
+{
+public:
+	std::string sample()
+	{
+		return {};
+	}
+};
+
+template < Fruit F >
+void test( F f )
+{
+	std::cout << f.type() << std::endl;
+}
+
 int main( int argc, char* argv[] )
 {
+	Apple a;
+	Banana b;
+
+	test( a );
+	// test( b ); // Won't compile
+
 	return 0;
 }
